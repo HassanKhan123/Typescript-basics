97cf06798a39b084e4af45604f1ce939e5ea9bad
HassanKhan123 <hk2602809@gmail.com>
2020-08-08 14:22:59 +0500

    string and boolean types added

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var add = function (num1, num2) {
-    return num1 + num2;
+var add = function (num1, num2, showResult, phrase) {
+    var result = num1 + num2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 };
 var num1 = 2;
 var num2 = 9;
-var result = add(num1, num2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is: ";
+add(num1, num2, printResult, resultPhrase);
