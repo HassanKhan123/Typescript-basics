440ad86e2cedec569c871c19132347b9ae7a8b06
HassanKhan123 <hk2602809@gmail.com>
2020-08-09 13:13:36 +0500

    tuple type added

--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
 // const obj: { name: string; age: number } = {
-    const obj = {
+const obj: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Hassan",
   age: 30,
-  hobbies:['Cricket','Coding']
+  hobbies: ["Cricket", "Coding"],
+  role: [2, "author"],
 };
 
 console.log(obj.age);
 
-for(const hobby of obj.hobbies){
-    console.log(hobby.toUpperCase())
+for (const hobby of obj.hobbies) {
+  console.log(hobby.toUpperCase());
 }
