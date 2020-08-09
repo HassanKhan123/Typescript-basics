bd6e95667e864d09a5501c8334b276f21320199d
HassanKhan123 <hk2602809@gmail.com>
2020-08-09 13:04:20 +0500

    array type added

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // const obj: { name: string; age: number } = {
 var obj = {
     name: "Hassan",
-    age: 30
+    age: 30,
+    hobbies: ['Cricket', 'Coding']
 };
 console.log(obj.age);
+for (var _i = 0, _a = obj.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
