d19b009b7bce5c3c3efaa7160ccb3f36f52ad707
HassanKhan123 <hk2602809@gmail.com>
2020-08-09 14:47:41 +0500

    unknown type

--- app.ts
+++ app.ts
@@ -1,23 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput:unknown;
+let userName:string
+
+userInput=5;
+userInput='Hassan'
+
+if(typeof userInput === 'string'){
+    userName=userInput
 }
-
-function printResult(num: number) {
-  console.log("Result is: " + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(1, 2));
-
-let combinedValues: (a: number, b: number) => number;
-combinedValues = add;
-
-console.log(combinedValues(8, 1));
-
-addAndHandle(10,20,(res) => {
-    console.log(res)
-})
