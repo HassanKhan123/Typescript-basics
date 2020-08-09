a92572cb8c092ca0d6a0db873c22c719e393cc50
HassanKhan123 <hk2602809@gmail.com>
2020-08-09 13:23:33 +0500

    enum type added

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 // const obj: { name: string; age: number } = {
+// const obj: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Hassan",
+//   age: 30,
+//   hobbies: ["Cricket", "Coding"],
+//   role: [2, "author"],
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var obj = {
     name: "Hassan",
     age: 30,
     hobbies: ["Cricket", "Coding"],
-    role: [2, "author"]
+    role: Role.ADMIN
 };
 console.log(obj.age);
 for (var _i = 0, _a = obj.hobbies; _i < _a.length; _i++) {
