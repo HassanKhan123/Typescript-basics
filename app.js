29a276ba009e00ae961219cc8d1a66ef0046de19
HassanKhan123 <hk2602809@gmail.com>
2020-08-09 12:52:07 +0500

    object type added

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-var add = function (num1, num2, showResult, phrase) {
-    var result = num1 + num2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
+// const obj: { name: string; age: number } = {
+var obj = {
+    name: "Hassan",
+    age: 30
 };
-var num1 = 2;
-var num2 = 9;
-var printResult = true;
-var resultPhrase = "Result is: ";
-add(num1, num2, printResult, resultPhrase);
+console.log(obj.age);
